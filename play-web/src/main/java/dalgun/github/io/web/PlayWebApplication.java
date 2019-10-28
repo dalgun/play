f08490b2cf9d03ff4f19c00b96e7957c80038b36
Dalma Gwon <dalma.gwon@lemonhc.com>
2019-10-28 18:01:05 +0900

    Multi Module 구조로 변경

---
+++ play-web/src/main/java/dalgun/github/io/web/PlayWebApplication.java
@@ -0,0 +1,13 @@
+package dalgun.github.io.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayWebApplication.class, args);
+	}
+
+}
