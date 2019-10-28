5a5c12223411409f57a64f3dcd19401c44173560
Dalma Gwon <dalma.gwon@lemonhc.com>
2019-10-28 10:12:26 +0900

    Multi Module 구조로 변경

--- play-refactoring/src/main/java/dalgun/github/io/refactoring/PlayRefactoringApplication.java
+++ play-refactoring/src/main/java/dalgun/github/io/refactoring/PlayRefactoringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RefactoringApplication {
+public class PlayRefactoringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RefactoringApplication.class, args);
+        SpringApplication.run(PlayRefactoringApplication.class, args);
     }
 
 }
