e7bfa33dd057a09b09295d9189a5b0fbf7d136f7
dalgun <dalgun@icloud.com>
2019-11-06 01:26:03 +0900

    bug fix

--- play-web/src/main/java/dalgun/github/io/web/domain/ConsultingReservationBase.java
+++ play-web/src/main/java/dalgun/github/io/web/domain/ConsultingReservationBase.java
@@ -15,7 +15,7 @@ public class ConsultingReservationBase extends ReservationBase {
     ConsultingReservationBase(){ super(ReservationType.CONSULTING);}
 
 //    @NotEmpty(message = "consultation type 은 FAMILY, FRIEND, COMPANY 중에 선택하셔야 합니다")
-    private ConsultationType consultationType;
+    private ConsultingType consultingType;
 
 //    @NotEmpty(message = "memo 는 null 이 아니며, 최소 10자리에서 최대 500자리까지 기입하셔야 합니다")
     @Size(min = 10, max = 500)
